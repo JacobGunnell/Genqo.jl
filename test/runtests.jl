0794403044017a4b4328c1c2ab86b0b664eddbb5
Jacob Gunnell <jacob.r.gunnell@gmail.com>
2025-12-24 11:31:06 -0800

    test for nan in zalm testset

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,7 +2,7 @@ using Genqo
 using Test
 
 @testset "ZALM" begin
-    @test_nowarn zalm.spin_density_matrix(1e-4, 0.9, 0.6, 0.8, [1,0,1,1,0,0,1,0])
-    @test_nowarn zalm.probability_success(1e-2, 0.8, 0.6, 0.9, 0.2)
-    @test_nowarn zalm.fidelity(1e-2, 0.8, 0.6, 0.9)
+    @test !any(isnan, zalm.spin_density_matrix(1e-4, 0.9, 0.6, 0.8, [1,0,1,1,0,0,1,0]))
+    @test !isnan(zalm.probability_success(1e-2, 0.8, 0.6, 0.9, 0.2))
+    @test !isnan(zalm.fidelity(1e-2, 0.8, 0.6, 0.9))
 end
