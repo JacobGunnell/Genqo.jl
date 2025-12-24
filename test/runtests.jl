5a3a75d96883b87d1cd640cde0e2c05b23cf194d
Jacob Gunnell <jacob.r.gunnell@gmail.com>
2025-12-24 11:20:11 -0800

    add zalm tests to runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,3 +1,8 @@
+using Genqo
 using Test
 
-@test 1==1
+@testset "ZALM" begin
+    @test_nowarn zalm.spin_density_matrix(1e-4, 0.9, 0.6, 0.8, [1,0,1,1,0,0,1,0])
+    @test_nowarn zalm.probability_success(1e-2, 0.8, 0.6, 0.9, 0.2)
+    @test_nowarn zalm.fidelity(1e-2, 0.8, 0.6, 0.9)
+end
