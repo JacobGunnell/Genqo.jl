60fb5449c51ffbaaee75cb5b49960f767a47477b
Josh McConkie <joshuamcconkie@gmail.com>
2025-12-23 19:41:26 -0700

    refactored fidelity to use new W function

--- src/fidelity.jl
+++ src/fidelity.jl
@@ -12,30 +12,31 @@ function fidelity(zalm::ZALM)
     
     # Define the matrix element
     # Python: Cn1 = ZALM.moment_vector([1], 1)
+    Cn0 = moment_vector([1], 0)
     Cn1 = moment_vector([1], 1)
     Cn2 = moment_vector([1], 2)
     Cn3 = moment_vector([1], 3)
     Cn4 = moment_vector([1], 4)
 
     # The loss matrix will be unique for calculating the fidelity    
-    zalm.loss_bsm_matrix_fid = loss_bsm_matrix_fid(zalm)
-    zalm.k_function_matrix = k_function_matrix(zalm)
+    L1 = loss_bsm_matrix_fid(zalm)
+    K1 = k_function_matrix(zalm)
 
-    nA1 = zalm.k_function_matrix + zalm.loss_bsm_matrix
-    Anv = inv(nA1)
+    nA1 = K1 + L1
+    Anv1 = inv(nA1)
 
     # ---- Compute W terms ----
-    F1 = W(Cn1, nA1, zalm.basisv)
-    F2 = W(Cn2, nA1, zalm.basisv)
-    F3 = W(Cn3, nA1, zalm.basisv)
-    F4 = W(Cn4, nA1, zalm.basisv)
+    F1 = W(Cn1, Anv1)
+    F2 = W(Cn2, Anv1)
+    F3 = W(Cn3, Anv1)
+    F4 = W(Cn4, Anv1)
 
     # Now calculate the trace of the state, which is equivalent to the probability of generation
-    zalm.loss_bsm_matrix = calculate_loss_bsm_matrix_pgen(zalm)
-    zalm.k_function_matrix = k_function_matrix(zalm)
-    
-    nA2 = zalm.k_function_matrix + zalm.loss_bsm_matrix
-    
+    L2 = loss_bsm_matrix_pgen(zalm)
+    K2 = k_function_matrix(zalm)
+
+    nA2 = K2 + L2
+
     N1 = ((zalm.detection_efficiency^2) * (zalm.outcoupling_efficiency^2))^2
     N2 = sqrt(det(nA2))
 
@@ -44,9 +45,8 @@ function fidelity(zalm::ZALM)
     #   That matches NumPy broadly, but can change phase if det moves around.
     D1 = sqrt(det(nA1))
 
-    coef = N1*N2/(2*D1)
-    Trc = W(Cn0, nA2, zalm.basisv)
-    fid = coef * (F1 + F2 + F3 + F4) / Trc
-    zalm.results["fidelity"] = fid
-    return fid
+    coef = N1 * N2 / (2*D1)
+    Trc = W(Cn0, nA2)
+
+    return coef * (F1 + F2 + F3 + F4) / Trc
 end
